ecf9e19f1ea0101534e119e13ea11006df263e12
ThinhHB <hbthinh176177@gmail.com>
2016-04-30 23:40:39 +0700

    + add MovementConfig.cs : main ScriptableObject for this demo + create 2 basic MovementConfig asset

---
+++ Assets/Script/MovementConfig.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[CreateAssetMenu(fileName = "MovementConfig", menuName = "GameConfiguration/PlayerMovement", order = 1)]
+public class MovementConfig : ScriptableObject
+{
+	public float movingAcceleration;
+	public float limitMovingSpeed;
+	public float jumpSpeed;
+}
